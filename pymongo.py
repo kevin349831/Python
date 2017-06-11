3b5a94494a247d3eb1a22b62ab6cfd32daf12552
kevin349831 <kevin349831@gmail.com>
2017-06-11 22:21:24 +0800

    Delete pymongo.py

--- pymongo.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-from pymongo import MongoClient
-
-client = pymongo.MongoClient("mongodb://kevin349831:33975712@ds119772.mlab.com:19772/mymongodb")
-
-db = client["mymongodb"]
-
-collection = db.test_collection
